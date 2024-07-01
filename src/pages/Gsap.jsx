551988e2e372d514c6a5fd129dcb76c7b77e0928
ohjunseok <ojspp000@naver.com>
2024-07-01 14:54:13 +0900

    make_page

---
+++ src/pages/Gsap.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Gsap() {
+  return (
+    <div>Gsap</div>
+  )
+}
---
+++ src/pages/Webd.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function webd() {
+  return (
+    <div>webd</div>
+  )
+}
