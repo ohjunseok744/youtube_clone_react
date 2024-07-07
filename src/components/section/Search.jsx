9698c6e0e7909dbf7ba30a9c09fbb96050b32575
ojspp000@naver.com <ojspp000@naver.com>
2024-07-08 01:55:44 +0900

    drdtr

--- src/components/section/Search.jsx
+++ src/components/section/Search.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
+    const [query, setQuery] = useState('');
+    const navigate = useNavigate();
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        if (query.trim()) {
+            navigate(`/search/${query}`);
+        }
+    };
+
     return (
         <div id='search'>
             <div className='search__inner'>
-                <label htmlFor='searchInput'>
-                    <span className='ir'>검색</span>
-                </label>
-                <input 
-                    type='search' 
-                    id='searchInput' 
-                    placeholder='🍳검색어를 입력해주세요' 
-                    autoComplete='off' 
-                    className='search__input' 
-                />
+                <form onSubmit={handleSearch}>
+                    <label htmlFor='searchInput'>
+                        <span className='ir'>검색</span>
+                    </label>
+                    <input 
+                        type='search' 
+                        id='searchInput' 
+                        placeholder='🍳검색어를 입력해주세요' 
+                        autoComplete='off' 
+                        className='search__input' 
+                        value={query}
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+                </form>
             </div>
         </div>
-    )
+    );
 }
+
 export default Search;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import Main from '../components/section/Main'
-
+import VideoSearch from '../components/videos/VideoSearch'
+import { fetchFromAPI } from '../utils/api'
 const Search = () => {
+    const { searchId } = useParams();
+    const [ videos, setVideos ] = useState([]);
+    
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${searchId}`)
+            .then((data) => setVideos(data.items))
+            .catch((error) => console.error('Error fetching data:', error));
+    }, [searchId]);
+
     return (
         <Main 
             title = "유투브 검색"
             description="유튜브 검색 결과 페이지입니다.">
-            Search
+            
+            <section id='searchPage'>
+                <div className="video__inner search">
+                    <VideoSearch videos={videos} />
+                </div>
+            </section>
         </Main>
     )
 }
 
-export default Search;
+export default Search
